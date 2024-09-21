7de6864208087bd66ead418063b13867a2152770
Giosferrazza <78505462+Giosferrazza@users.noreply.github.com>
2024-09-21 16:41:36 +0000

    icon update

--- giovanni/src/components/Main.js
+++ giovanni/src/components/Main.js
@@ -18,32 +18,34 @@ export default function MainContent() {
 
   return (
     <main>
-      <section className="flex flex-col md:flex-row items-center mt-20 mr-48">
-        <img
-          src="/me.jpg"
-          alt="Profile"
-          className="w-75 h-80 rounded-lg mx-auto mb-4 md:mb-0 md:mr-8 mt-40"
-        />
-        <div className="text-center md:text-left">
-          <h2 className={`text-2xl font-bold mb-4 mt-11 ${showText ? styles.typing : ''}`}>
-            Hey, I'm Giovanni.
-          </h2>
-          <p className="text-lg mb-4">
-            A passionate developer with experience in building various applications.
-          </p>
-          <div className="flex flex-col items-center md:items-start space-y-4">
-            <a href="https://www.linkedin.com/in/your-profile" target="_blank" rel="noopener noreferrer" className="flex items-center text-blue-500 hover:text-blue-500 ">
-              <FaLinkedin size={30} />Linkedin
-            </a>
-            <a href="https://github.com/your-profile" target="_blank" rel="noopener noreferrer" className="flex items-center text-gray-800 hover:text-gray-600">
-              <FaGithub size={30} />
-            </a>
-            <a href="/path-to-your-resume.pdf" target="_blank" rel="noopener noreferrer" className="flex items-center text-gray-800 hover:text-gray-700">
-              <HiDocument size={30} />
-            </a>
+      <div className={styles['centered-container']}>
+        <section className="flex flex-col md:flex-row items-center mt-20 ">
+          <img
+            src="/me.jpg"
+            alt="Profile"
+            className="w-75 h-80 rounded-lg mx-auto mb-4 md:mb-0 md:mr-8 mt-40"
+          />
+          <div className="text-center md:text-left">
+            <h2 className={`text-2xl font-bold mb-4 mt-11 ${showText ? styles.typing : ''}`}>
+              Hey, I'm Giovanni.
+            </h2>
+            <p className="text-lg mb-4">
+              A passionate developer with experience in building various applications.
+            </p>
+            <div className="flex flex-col items-center md:items-start space-y-4 text-white">
+              <a href="https://www.linkedin.com/in/your-profile" target="_blank" rel="noopener noreferrer" className="flex items-center text-blue-500 hover:text-blue-500 ">
+                <FaLinkedin size={30} className="text-blue-500 mr-2"/>Linkedin
+              </a>
+              <a href="https://github.com/your-profile" target="_blank" rel="noopener noreferrer" className="flex items-center">
+                <FaGithub size={30} className="text-gray-500 mr-2"/>Github
+              </a>
+              <a href="/path-to-your-resume.pdf" target="_blank" rel="noopener noreferrer" className="flex items-center">
+                <HiDocument size={30} className="text-gray-500 mr-2"/>Resume
+              </a>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </main>
   );
 }
