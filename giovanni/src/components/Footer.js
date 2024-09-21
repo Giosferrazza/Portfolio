6a6572617bfed1129ba0d3cb98bbd284ad439824
Giosferrazza <78505462+Giosferrazza@users.noreply.github.com>
2024-09-21 04:37:08 +0000

    centered footer

--- giovanni/src/components/Footer.js
+++ giovanni/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="p-4 text-center">
+    <footer className="left-0 w-full p-4 text-center text-white">
       <p>&copy; {new Date().getFullYear()} Giovanni Sferrazza. All rights reserved.</p>
     </footer>
   );
