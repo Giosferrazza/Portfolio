e7b41274cc1e165dabc3becf7fa30fecdd24b00f
Giosferrazza <78505462+Giosferrazza@users.noreply.github.com>
2024-09-21 16:25:25 +0000

    animation for h1

--- giovanni/src/app/page.js
+++ giovanni/src/app/page.js
@@ -4,7 +4,7 @@ import Head from 'next/head';
 import Header from '../components/Header';
 import Main from '../components/Main';
 import Footer from '../components/Footer';
-import './index.css';
+import '../styles/globals.css';  
 
 export default function Home() {
   return (
--- giovanni/src/components/Main.js
+++ giovanni/src/components/Main.js
@@ -1,33 +1,45 @@
-import React, { useEffect } from 'react';
-import { FaLinkedin, FaGithub, FaFileAlt } from 'react-icons/fa';
+"use client";
 
-function Main() {
+import { useEffect, useState } from 'react';
+import { FaLinkedin, FaGithub } from 'react-icons/fa'; // Import GitHub icon
+import { HiDocument } from 'react-icons/hi'; // Import resume icon
+import styles from '../styles/Main.module.css'; // Import CSS module
+
+export default function MainContent() {
+  const [showText, setShowText] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowText(true);
+    }, 300); 
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <main className="flex items-start justify-center min-h-screen p-4 font-mono">
-      <section className="flex flex-col md:flex-row items-center mt-20">
+    <main>
+      <section className="flex flex-col md:flex-row items-center mt-20 mr-48">
         <img
           src="/me.jpg"
           alt="Profile"
           className="w-75 h-80 rounded-lg mx-auto mb-4 md:mb-0 md:mr-8 mt-40"
         />
         <div className="text-center md:text-left">
-          <h2 className="text-2xl font-bold mb-4 mt-11">Hey, I'm Giovanni.</h2>
+          <h2 className={`text-2xl font-bold mb-4 mt-11 ${showText ? styles.typing : ''}`}>
+            Hey, I'm Giovanni.
+          </h2>
           <p className="text-lg mb-4">
             A passionate developer with experience in building various applications.
           </p>
           <div className="flex flex-col items-center md:items-start space-y-4">
-            <a href="https://www.linkedin.com/in/your-profile" target="_blank" rel="noopener noreferrer" className="flex items-center text-blue-500 hover:text-blue-700">
-              <FaLinkedin size={30} />
-              <span className="ml-2 text-white">LinkedIn</span>
+            <a href="https://www.linkedin.com/in/your-profile" target="_blank" rel="noopener noreferrer" className="flex items-center text-blue-500 hover:text-blue-500 ">
+              <FaLinkedin size={30} />Linkedin
             </a>
-            <a href="https://github.com/your-username" target="_blank" rel="noopener noreferrer" className="flex items-center text-gray-800 hover:text-gray-600">
+            <a href="https://github.com/your-profile" target="_blank" rel="noopener noreferrer" className="flex items-center text-gray-800 hover:text-gray-600">
               <FaGithub size={30} />
-              <span className="ml-2 text-white">GitHub</span>
             </a>
-            <a href="/path/to/your/resume.pdf" target="_blank" rel="noopener noreferrer" className="flex items-center text-gray-800 hover:text-gray-600">
-              <FaFileAlt size={30} />
-              <span className="ml-2 text-white">Resume</span>
+            <a href="/path-to-your-resume.pdf" target="_blank" rel="noopener noreferrer" className="flex items-center text-gray-800 hover:text-gray-700">
+              <HiDocument size={30} />
             </a>
           </div>
         </div>
@@ -35,5 +47,3 @@ function Main() {
     </main>
   );
 }
-
-export default Main;
