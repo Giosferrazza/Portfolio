be9352065cf92101042088d8856feaa5fb1a303a
Giosferrazza <78505462+Giosferrazza@users.noreply.github.com>
2024-09-21 03:43:33 +0000

    Header and mono font for style

--- giovanni/src/components/Header.js
+++ giovanni/src/components/Header.js
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState } from 'react';
 
 function Header() {
@@ -8,34 +10,14 @@ function Header() {
   };
 
   return (
-    <header className="bg-gray-800 p-4 text-white text-center">
-      <h1 className="text-3xl font-bold">Giovanni Sferrazza</h1>
-      <button
-        onClick={toggleMenu}
-        className="hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-        aria-controls="navbar-hamburger"
-        aria-expanded={isMenuOpen}
-      >
-        <span className="sr-only">Open main menu</span>
-        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
-        </svg>
-      </button>
-      <div className={`${isMenuOpen ? 'block' : 'hidden'} w-full`} id="navbar-hamburger">
-        <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-          <li>
-            <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded dark:bg-blue-600" aria-current="page">Home</a>
-          </li>
-          <li>
-            <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Services</a>
-          </li>
-          <li>
-            <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white">Pricing</a>
-          </li>
-          <li>
-            <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Contact</a>
-          </li>
-        </ul>
+    <header className="text-white p-5">
+      <div className="flex items-center justify-between">
+        <h1 className="text-3xl font-mono ">Giovanni Sferrazza</h1>
+        <nav className="flex space-x-3">
+          <a href="/" className="hover:underline font-mono text-2xl">Home</a>
+          <a href="/projects" className="hover:underline font-mono text-2xl">Projects</a>
+          <a href="/contact" className="hover:underline font-mono text-2xl">Contact</a>
+        </nav>
       </div>
     </header>
   );
