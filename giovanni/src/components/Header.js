4cf6b273a3889d32870a6cb65138001bd254d4e7
Giosferrazza <78505462+Giosferrazza@users.noreply.github.com>
2024-09-21 16:02:05 +0000

    Added Blog section

--- giovanni/src/components/Header.js
+++ giovanni/src/components/Header.js
@@ -6,11 +6,11 @@ function Header() {
   return (
     <header className="text-white p-5">
       <div className="flex items-center justify-between">
-        <h1 className="text-3xl font-mono ">Giovanni Sferrazza</h1>
+        <h1 className="text-3xl font-mono "><a href="/Home" className="hover:underline">Giovanni Sferrazza</a></h1>
         <nav className="flex space-x-3">
-          <a href="/" className="hover:underline font-mono text-2xl">Home</a>
           <a href="/projects" className="hover:underline font-mono text-2xl">Projects</a>
           <a href="/contact" className="hover:underline font-mono text-2xl">Contact</a>
+          <a href="/Blog" className="hover:underline font-mono text-2xl">Blog</a>
         </nav>
       </div>
     </header>
