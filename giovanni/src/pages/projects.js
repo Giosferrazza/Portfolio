311b6b845d270dea3de4177cde9938a8ca4d7f1b
Giosferrazza <78505462+Giosferrazza@users.noreply.github.com>
2024-09-24 03:27:14 +0000

    changed bg color removed main from the projects.js file to leave room to implement project buttons

--- giovanni/src/pages/projects.js
+++ giovanni/src/pages/projects.js
@@ -1,11 +1,20 @@
-// src/pages/projects.js
+import Head from 'next/head';
+import Header from '../components/Header';
+import Main from '../components/Main';
+import Footer from '../components/Footer';
+import '../styles/globals.css';
+import '../styles/project.module.css'
+
 import React from 'react';
 
 function Projects() {
   return (
-    <div>
-      <h1>Projects Page</h1>
-      <p>This is the projects page.</p>
+    <div className="flex flex-col min-h-screen bg-black text-white">
+      <Head>
+        <title>Giovanni Sferrazza</title>
+      </Head>
+      <Header />
+      <Footer />
     </div>
   );
 }
